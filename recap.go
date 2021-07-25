89deec2bb0cf7ced939f1c0382d6d8918aaf50ca
JoelKanyi <joelkanyi98@gmail.com>
2021-07-25 10:23:02 +0300

    recap

---
+++ recap.go
@@ -0,0 +1,26 @@
+package main
+
+
+var balance = 2000
+func main() {
+	println(checkBalance())
+	depositMoney(1000)
+	println(checkBalance())
+	withdrawMoney(100)
+}
+
+func checkBalance()int{
+	return balance
+}
+
+func depositMoney(amount int){
+	balance = balance + amount
+}
+
+func withdrawMoney(amount int){
+	if amount <= balance{
+		balance = balance - amount
+	}else{
+		print("You have insufficient funds")
+	}
+}
